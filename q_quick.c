bdcdb682e8d7e2abed99ed3bd8a71ac77dc8de2f
hus-king <3334991525@qq.com>
2024-11-29 18:50:03 +0800

    添加哈希表实现，优化字符串查找功能并释放内存

--- q_quick.c
+++ q_quick.c
@@ -1,10 +1,53 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-void quicksort(char a[][20], int low, int high);
-int partition(char a[][20], int low, int high);
-int compare(const char *a, const char *b);
-int comparechar(char a, char b);
+#define HASH_SIZE 220000
+
+typedef struct HashNode {
+    char key[20];
+    struct HashNode *next;
+} HashNode;
+
+unsigned int hash(const char *str) {
+    unsigned int hash = 5381;
+    int c;
+    while ((c = *str++)) {
+        hash = ((hash << 5) + hash) + c;
+    }
+    return hash % HASH_SIZE;
+}
+
+void insert(HashNode **hashTable, const char *str) {
+    unsigned int index = hash(str);
+    HashNode *newNode = (HashNode *)malloc(sizeof(HashNode));
+    strcpy(newNode->key, str);
+    newNode->next = hashTable[index];
+    hashTable[index] = newNode;
+}
+
+int search(HashNode **hashTable, const char *str) {
+    unsigned int index = hash(str);
+    HashNode *node = hashTable[index];
+    while (node) {
+        if (strcmp(node->key, str) == 0) {
+            return 1;
+        }
+        node = node->next;
+    }
+    return 0;
+}
+
+void freeHashTable(HashNode **hashTable) {
+    for (int i = 0; i < HASH_SIZE; i++) {
+        HashNode *node = hashTable[i];
+        while (node) {
+            HashNode *temp = node;
+            node = node->next;
+            free(temp);
+        }
+    }
+}
 
 int main() {
     int n, m, k;
@@ -13,22 +56,22 @@ int main() {
     int flag[110000]; // n
     char full[110000][20]; // m
     char new1[110000][20]; // k
+    HashNode *hashTable[HASH_SIZE] = {0};
+
     for (int i = 0; i < n; i++) {
         scanf("%s", old[i]);
         flag[i] = 0; // 0为要输出
     }
     for (int i = 0; i < m; i++) {
         scanf("%s", full[i]);
+        insert(hashTable, full[i]);
     }
     for (int i = 0; i < k; i++) {
         scanf("%s", new1[i]);
     }
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (strcmp(old[i], full[j]) == 0) {
-                flag[i] = 1;
-                break;
-            }
+        if (search(hashTable, old[i])) {
+            flag[i] = 1;
         }
     }
     char putout[210000][20];
@@ -47,6 +90,8 @@ int main() {
     for (int i = 0; i < j; i++) {
         puts(putout[i]);
     }
+
+    freeHashTable(hashTable);
 }
 
 void quicksort(char a[][20], int low, int high) {
@@ -62,7 +107,7 @@ int partition(char a[][20], int low, int high) {
     strcpy(pivot, a[high]);
     int i = (low - 1);
     for (int j = low; j <= high - 1; j++) {
-        if (compare(a[j], pivot) < 0) {
+        if (strcmp(a[j], pivot) < 0) {
             i++;
             char temp[20];
             strcpy(temp, a[i]);
@@ -75,32 +120,4 @@ int partition(char a[][20], int low, int high) {
     strcpy(a[i + 1], a[high]);
     strcpy(a[high], temp);
     return (i + 1);
-}
-
-int compare(const char *a, const char *b) {
-    int i = 0;
-    while (a[i] != '\0' && b[i] != '\0') {
-        int result = comparechar(a[i], b[i]);
-        if (result != 0) {
-            return result;
-        }
-        i++;
-    }
-    if (a[i] == '\0' && b[i] == '\0') {
-        return 0;
-    } else if (a[i] == '\0') {
-        return -1;
-    } else {
-        return 1;
-    }
-}
-
-int comparechar(char a, char b) {
-    if (a == b) {
-        return 0;
-    } else if (a > b) {
-        return 1;
-    } else {
-        return -1;
-    }
 }
